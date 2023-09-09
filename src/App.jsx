408d2a0d9f6920b46804d341d56c25caa90e4c64
Prabhat-022 <lograjanand1250@gmail.com>
2023-09-09 07:03:09 +0530

    New feature add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
-import Name from './Practice/Name'
+import Card from './Tailwind/Card'
+// import Name from './Practice/Name'
 
 
 function App() {
+  let myObj = {
+    username: "hitesh",
+    age: 21
+  }
 
   return (
     <>
-    <Name/>
+      <h1 className="bg-green-400 text-center m-7">
+        Hello world!
+      </h1>
+      <div className="flex ml-10  p-4">
+        {/* <Card  usernames ="Surbhi" channel ="chaiaurcode" someObj={myObj} /> */}
+        <Card username="surbhi" btnTxt ="Visit me"/>
+        <Card username="langofrod" btnTxt =" Click Me"/>
+        <Card username="jenofer" />
+        <Card username="Abla" />
+      </div>
+      {/* <Name/> */}
     </>
   )
 }
